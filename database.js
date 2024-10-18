c5cba53781cdca9883a82baccdd523a0f4496011
DESKTOP-OA1VINS\PC <dunghq@sparkminds.net>
2024-10-18 12:26:16 +0700

    dev done

--- database.js
+++ database.js
@@ -4,6 +4,8 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect('mongodb+srv://dung2001:gW8LFefR6btLdxwJ@cluster0.qsmoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
         });
+        // const conn = await mongoose.connect('mongodb://localhost:27017/volunteer_blog', {
+        // });
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error('MongoDB connection error:', error);
